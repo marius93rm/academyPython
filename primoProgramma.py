8210b142620af561d29f4364be4ac960a3f3e9d6
kate14845@gmail.com <kate14845@gmail.com>
2021-03-01 16:36:54 +0100

    per dar fastidio

--- primoProgramma.py
+++ primoProgramma.py
@@ -18,8 +18,4 @@ print("banana")
 # la modifica definitiva
 print("modifiva semi-definitiva")
 
-<<<<<<< HEAD
-print("HI")
-=======
 print("Grandi così")
->>>>>>> 1e440862e0e779ca7d34c3c0c5bfd78d0778d7d4
