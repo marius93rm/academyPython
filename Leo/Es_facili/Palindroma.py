65bd5182349bce95a6bd658927bad16fd2b3b638
LUrbinati <s254074@studenti.polito.it>
2021-03-04 10:45:08 +0100

    eliminacartella

--- Leo/Es_facili/Palindroma.py
+++
@@ -1,9 +0,0 @@
-#Inserire una parola in input e verificare se è palindroma
-
-parola=input("Inserisci una parola: ")
-
-lunghezza=len(parola)
-for i in range(lunghezza):
-    if parola[i]!=parola[-i-1]:
-        print("Non è palindroma")
-        break
